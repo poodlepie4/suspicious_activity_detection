316f204736ca2d6c3d6d46293d801b7db26dd1e3
poodlepie4 <160336978+poodlepie4@users.noreply.github.com>
2026-03-15 18:41:16 +0530

    download_dataset.py

---
+++ download_dataset.py
@@ -0,0 +1,10 @@
+import kagglehub
+import os
+
+# download dataset
+path = kagglehub.dataset_download("odins0n/ucf-crime-dataset")
+
+print("Dataset downloaded to:", path)
+
+# list files
+print(os.listdir(path))
