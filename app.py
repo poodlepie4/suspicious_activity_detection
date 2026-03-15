5c99a824c387a56a1501548fa4143503db48975c
poodlepie4 <160336978+poodlepie4@users.noreply.github.com>
2026-03-15 22:18:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template, request
+import pickle
+
 app = Flask(__name__)
+
+model = pickle.load(open("model/model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
