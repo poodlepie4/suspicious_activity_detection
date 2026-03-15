252214442ba4ce08358ce06202c53c791d867ac6
poodlepie4 <160336978+poodlepie4@users.noreply.github.com>
2026-03-16 00:27:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,5 @@ def predict():
 
     return render_template("index.html", prediction_text=result)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+   if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
