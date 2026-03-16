d19eff87a161f274eccc5189caae8f1529e81035
poodlepie4 <160336978+poodlepie4@users.noreply.github.com>
2026-03-16 22:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,35 +4,28 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load trained model
 model = pickle.load(open("model/model.pkl", "rb"))
 
-# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# Prediction route
 @app.route("/predict", methods=["POST"])
 def predict():
-    
-    # Get values from form
+
     features = [float(x) for x in request.form.values()]
-    
-    # Convert to numpy array
     final_features = np.array(features).reshape(1, -1)
 
-    # Model prediction
     prediction = model.predict(final_features)
 
-    # Convert result to readable output
     if prediction[0] == 1:
         result = "Suspicious Activity Detected"
     else:
         result = "Normal Activity"
 
-    return render_template("index.html", prediction_text=result)
+    return render_template("index.html", prediction=result)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
